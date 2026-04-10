ad2fadfd59730fa76107f282ae3e92a8342e63b4
Stepan <zubbodazdeban@mail.ru>
2026-04-10 21:18:26 +0300

    Обновил EmptyParserStrategy

--- Laba2/src/main/java/parser/EmptyParserStrategy.java
+++ Laba2/src/main/java/parser/EmptyParserStrategy.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 /**
  * @author zubbo
  */
-public class PipeParserStrategy implements ParserStrategy {
+public class EmptyParserStrategy implements ParserStrategy {
     
     @Override
     public boolean supports(File file) {
